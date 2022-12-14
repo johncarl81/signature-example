3acf34db1b16d06816963ea53fdd4622900d5a45
John Ericksen <johncarl81@gmail.com>
2022-12-14 09:31:19 -0700

    Setup signature example app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useRef, useState} from "react";
+import SignaturePad from "signature_pad";
 
 function App() {
+
+  const [signature, setSignature] = useState();
+
+  const signatureRef = useRef(null);
+
+  useEffect(() => {
+    const signaturePad = new SignaturePad(signatureRef.current, {
+      backgroundColor: '#EFEFEF',
+      penColor: 'rgb(0, 0, 0)'
+    });
+
+    // Comment out the following to show the original issue:
+    const canvas = signatureRef.current;
+    const ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+
+    signaturePad.addEventListener('endStroke', function() {
+      setSignature(signaturePad.toDataURL("image/svg+xml"));
+    })
+  }, [signatureRef]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div><canvas id="signature-pad" ref={signatureRef} className="signature-pad" width="500" height="150"/></div>
+        <img src={signature} height="150" width="500"/>
       </header>
     </div>
   );
